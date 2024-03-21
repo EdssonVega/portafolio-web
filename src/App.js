634b12e2047258ed1e1be35f8f68a3401ad3dfdd
Edsson <e_d_sx2@hotmail.com>
2024-03-21 04:20:09 -0600

    adding projects section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import NavbarSection from './components/navbar/Navbar';
+import HeroSection from './components/Hero/Hero';
 import AboutMeSection from './components/AboutMe/AboutMe';
+import ProjectSection from './components/Projects/Projects';
 
 
 function App() {
   return (
     <div className="App">
       <NavbarSection />
+      <HeroSection />
       <AboutMeSection />
+      <ProjectSection/>
     </div>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Project.css"
+
+
+
+function ProjectSection(){
+    return(
+        <section className="projectsSection">
+
+        </section>
+    )
+}
+
+export default ProjectSection
